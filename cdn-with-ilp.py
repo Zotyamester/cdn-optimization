c151704f14e7a133088d4e23ed704c5344a89d89
Zotyamester <szatmary.zoltan1222@gmail.com>
2024-06-14 12:40:12 +0200

    Fix plotting of the network

--- cdn-with-ilp.py
+++ cdn-with-ilp.py
@@ -137,7 +137,7 @@ for link, (latency, cost) in links.items():
     g.add_edge(*link, latency=latency, cost=cost)
 
 plt.figure(figsize=(16, 10))
-node_positions = {node_name: node_attrs["location"] for node_name, node_attrs in g.nodes.data()}
+node_positions = {node_name: (node_attrs["location"][1], node_attrs["location"][0]) for node_name, node_attrs in g.nodes.data()}
 nx.draw_networkx(g, pos=node_positions, node_size=4000, font_size=10, font_color="white")
 nx.draw_networkx_edges(g, pos=node_positions, edgelist=used_links, edge_color="red", width=2)
 
