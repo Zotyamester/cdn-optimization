58498e7f06adeccb90d7aef62698e233a7c68d3c
Zotyamester <szatmary.zoltan1222@gmail.com>
2024-06-14 15:03:24 +0200

    Visualize edge directions in plots

--- plot.py
+++ plot.py
@@ -4,20 +4,21 @@ from model import Network
 
 
 def plot_network(network: Network, used_links: list[tuple[str, str]]):
-    g = nx.Graph()
+    g = nx.DiGraph()
 
     for node, (location, cost_factor) in network.nodes.items():
         g.add_node(node, location=location, cost_factor=cost_factor)
     for link, (latency, cost) in network.links.items():
         g.add_edge(*link, latency=latency, cost=cost)
 
-    plt.figure(figsize=(16, 10))
     node_positions = {node_name: (node_attrs["location"][1], node_attrs["location"][0])
                       for node_name, node_attrs in g.nodes.data()}
+
+    plt.figure(figsize=(16, 10))
     nx.draw_networkx(g, pos=node_positions, node_size=4000,
-                     font_size=10, font_color="white")
+                     font_size=10, font_color="white", arrowstyle="-")
     nx.draw_networkx_edges(g, pos=node_positions,
-                           edgelist=used_links, edge_color="red", width=2)
+                           edgelist=used_links, edge_color="red", width=3, arrowsize=15, node_size=3800)
 
     plt.axis("off")
     plt.show()
