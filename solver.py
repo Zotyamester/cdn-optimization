6aa4192df73c8291e81f6b94f28b1473204676a8
Zotyamester <szatmary.zoltan1222@gmail.com>
2024-07-12 13:05:44 +0200

    Encapsulate solutions of optimiztations

--- solver.py
+++ solver.py
@@ -1,9 +1,48 @@
+import dataclasses
+import itertools
+import math
 from typing import Callable
 import pulp as lp
 from model import Network, Track
 
 
-def get_optimal_topology_for_a_single_track(network: Network, track: Track, debug: bool = False) -> tuple[int, list[tuple[str, str]]]:
+class SingleTrackSolution:
+    def __init__(self, success: bool, objective: float, used_links: list[tuple[str, str]]):
+        self.success = success
+        self.objective = objective
+        self.used_links = used_links
+
+    def __iter__(self):
+        yield from (self.success, self.objective, self.used_links)
+
+    @staticmethod
+    def found(objective: float, used_links: list[tuple[str, str]]) -> 'SingleTrackSolution':
+        return SingleTrackSolution(True, objective, used_links)
+
+    @staticmethod
+    def not_found() -> 'SingleTrackSolution':
+        return SingleTrackSolution(False, 0.0, [])
+
+
+class MultiTrackSolution:
+    def __init__(self, success: bool, objective: float, used_links_per_track: dict[str, list[tuple[str, str]]]):
+        self.success = success
+        self.objective = objective
+        self.used_links_per_track = used_links_per_track
+
+    def __iter__(self):
+        yield from (self.success, self.objective, self.used_links_per_track)
+
+    @staticmethod
+    def found(objective: float, used_links_per_track: dict[str, list[tuple[str, str]]]) -> 'MultiTrackSolution':
+        return MultiTrackSolution(True, objective, used_links_per_track)
+
+    @staticmethod
+    def not_found() -> 'MultiTrackSolution':
+        return MultiTrackSolution(False, 0.0, {})
+
+
+def get_optimal_topology_for_a_single_track(network: Network, track: Track) -> SingleTrackSolution:
     prob = lp.LpProblem("MoQ_relay_topology_optimization", lp.LpMinimize)
 
     # xfij == x_{stream}_{link}; xfij >= 0 constraint is always satisfied
@@ -51,46 +90,134 @@ def get_optimal_topology_for_a_single_track(network: Network, track: Track, debu
 
     # Constraint: sum(zfij * dij) <= Df
     for stream, (delay_budget, _) in track.streams.items():
-        prob += lp.lpSum([selected_links[stream][link] * latency
-                          for link, (latency, _) in network.links.items()]) <= delay_budget, \
+        prob += lp.lpSum([selected_links[stream][link] * e2e_latency
+                          for link, (e2e_latency, _) in network.links.items()]) <= delay_budget, \
             f"delay_budget_for_{stream}"
 
     prob.solve(lp.PULP_CBC_CMD(msg=False))
 
-    status = prob.status
+    success = prob.status == lp.LpStatusOptimal
+    if not success:
+        return SingleTrackSolution.not_found()
 
-    if debug:
-        print(f"Solution: {lp.LpStatus[status]}")
-        print()
-
-        print("Variables:")
-        for var in prob.variables():
-            value = int(var.varValue)
-            if value > 0:
-                print(f" - {var.name} = {value}")
-        print()
-
-        print("Constraints:")
-        for constraint in prob.constraints.values():
-            if constraint.value() != 0:
-                print(f" - {constraint.name}: {constraint.value()}")
-        print()
-
+    objective = prob.objective.value()
     used_links = [link for link, var in link_usages.items()
                   if var.varValue > 0]
-    return status, used_links
+    return SingleTrackSolution.found(objective, used_links)
 
-def multi_to_single_track_adapter(network: Network, tracks: dict[str, Track], debug: bool = False) -> tuple[int, dict[str, list[tuple[str, str]]]]:
-    status = lp.const.LpStatusOptimal
+
+def find_cheapest_link_with_delay_constraint(network: Network, track: Track, tree_nodes: set[str], tree_latencies: dict[str, float]) -> tuple[str, str] | None:
+    best_link = None
+    best_cost = math.inf
+    
+    for node1 in tree_nodes:
+        for node2 in network.nodes.keys():
+            if node2 not in tree_nodes:
+                link = (node1, node2)
+                latency, cost = network.links[link]
+                e2e_latency = tree_latencies[node1] + latency
+                if e2e_latency < track._subscribers[node2] and cost < best_cost:
+                    best_link = (node1, node2)
+                    best_cost = cost
+
+    return best_link
+
+
+def find_link_with_max_delay_relaxation(network: Network, track: Track, tree_nodes: set[str], tree_latencies: dict[str, float]) -> tuple[str, str] | None:
+    best_link = None
+    max_relaxation = 0.0
+    
+    for link in itertools.combinations(tree_nodes, 2):
+        node1, node2 = link
+        latency, _ = network.links[link]
+
+        new_e2e_latency = tree_latencies[node1] + latency
+        old_e2e_latency = tree_latencies[node2]
+
+        relaxation = old_e2e_latency - new_e2e_latency
+        if relaxation > max_relaxation:
+            best_link = link
+            max_relaxation = relaxation
+
+    return best_link
+
+
+def bdb_for_a_single_track(network: Network, track: Track) -> SingleTrackSolution:
+    tree_nodes = {track._publisher}
+    tree_edges = set()
+    tree_latencies = {track._publisher: 0}
+    tree_cost = 0.0
+
+    # Phase 1: Construct a suboptimal delay-constrained spanning tree
+
+    while len(tree_nodes) < len(network.nodes):
+        if (link := find_cheapest_link_with_delay_constraint(network, track, tree_nodes, tree_latencies)) is not None:
+            node1, node2 = link
+            latency, cost = network.links[link]
+
+            tree_nodes.add(node2)
+            tree_edges.add(link)
+            tree_latencies[node2] = tree_latencies[node1] + latency
+            tree_cost += cost
+        elif (new_link := find_link_with_max_delay_relaxation(network, track, tree_nodes, tree_latencies)) is not None:
+            # Remove & clean up after the old link
+            old_link = next(filter(lambda link, end_node=node2: link[1] == end_node, tree_edges))
+            _, old_cost = network.links[old_link]
+
+            tree_edges.remove(old_link)
+            tree_cost -= old_cost
+
+            # Add the new link & update the tree
+            node1, node2 = new_link
+            new_latency, new_cost = network.links[new_link]
+
+            tree_edges.add(new_link)
+            tree_latencies[node2] = tree_latencies[node1] + new_latency
+            tree_cost += new_cost
+
+            # Update the tree latencies (in a BFS fashion)
+            update_queue = {node2}
+            while len(update_queue) > 0:
+                node = update_queue.pop()
+                for link in tree_edges:
+                    if link[0] == node:
+                        next_node = link[1]
+                        tree_latencies[next_node] = tree_latencies[node] + network.links[link].latency
+                        update_queue.add(next_node)
+        else:
+            return SingleTrackSolution.not_found()
+    
+    # Phase 2: Optimize the tree by replacing edges using heuristics
+
+    # TODO: Implement the heuristic
+    # while True:
+    #     link = find_cheapest_augmenting_link_with_delay_constraint(network, track, tree_nodes, tree_latencies)
+    #     if contains_directed_loop(track._publisher, tree_edges):
+    #         link_to_remove, link_to_add = find_replacement_pair_in_directed_loop(network, track, tree_nodes, tree_latencies)
+    #         if link_to_remove is None or link_to_add is None:
+    #             break
+    #         tree_edges.remove(link_to_remove)
+    #         tree_edges.add(link_to_add)
+    #         tree_cost += network.links[link_to_add].cost - network.links[link_to_remove].cost
+
+    return SingleTrackSolution.found(tree_cost, list(tree_edges))
+
+
+def multi_to_single_track_adapter(network: Network, tracks: dict[str, Track]) -> MultiTrackSolution:
+    objective = 0.0
     used_links_per_track = {}
     for track_id, track in tracks.items():
-        track_status, used_links = get_optimal_topology_for_a_single_track(network, track, debug)
-        used_links_per_track[track_id] = used_links
-        if track_status != lp.const.LpStatusOptimal:
-            status = track_status
-    return status, used_links_per_track
+        track_success, track_objective, used_links = get_optimal_topology_for_a_single_track(
+            network, track)
+        if not track_success:
+            return MultiTrackSolution.not_found()
 
-def get_optimal_topology_for_multiple_tracks(network: Network, tracks: dict[str, Track], debug: bool = False) -> tuple[int, dict[str, list[tuple[str, str]]]]:
+        objective += track_objective
+        used_links_per_track[track_id] = used_links
+    return MultiTrackSolution.found(objective, used_links_per_track)
+
+
+def get_optimal_topology_for_multiple_tracks(network: Network, tracks: dict[str, Track]) -> MultiTrackSolution:
     prob = lp.LpProblem("MoQ_relay_topology_optimization", lp.LpMinimize)
 
     # xftij == x_{track}_{stream}_{link}; xftij >= 0 constraint is always satisfied
@@ -110,7 +237,7 @@ def get_optimal_topology_for_multiple_tracks(network: Network, tracks: dict[str,
             f"z_{track_id}", (track.streams.keys(), network.links.keys()), 0, 1, cat=lp.LpBinary)
 
     # Objective function
-    prob += lp.lpSum([cost * link_usages[track_id][link] for link, (_, cost, _)
+    prob += lp.lpSum([cost * link_usages[track_id][link] for link, (_, cost)
                      in network.links.items() for track_id in tracks.keys()]), "total_link_usage"
 
     # Constraint: ytij >= xftij
@@ -147,43 +274,25 @@ def get_optimal_topology_for_multiple_tracks(network: Network, tracks: dict[str,
     # Constraint: sum(zftij * dij) <= Dft
     for track_id, track in tracks.items():
         for stream, (delay_budget, _) in track.streams.items():
-            prob += lp.lpSum([selected_links[track_id][stream][link] * latency
-                              for link, (latency, _, _) in network.links.items()]) <= delay_budget, \
+            prob += lp.lpSum([selected_links[track_id][stream][link] * e2e_latency
+                              for link, (e2e_latency, _) in network.links.items()]) <= delay_budget, \
                 f"delay_budget_for_{track_id}_{stream}"
-
-    # # Constraint: sum(ytij) <= Cij
-    # for link in network.links.keys():
-    #     prob += lp.lpSum([link_usages[track_id][link]
-    #                       for track_id in tracks.keys()]) <= network.links[link].capacity, f"link_capacity_for_({link[0]},{link[1]})"
 
     prob.solve(lp.PULP_CBC_CMD(msg=False))
 
-    status = prob.status
+    success = prob.status == lp.LpStatusOptimal
+    if not success:
+        return MultiTrackSolution.not_found()
 
-    if debug:
-        print(f"Solution: {lp.LpStatus[status]}")
-        print()
-
-        print("Variables:")
-        for var in prob.variables():
-            value = int(var.varValue)
-            if value > 0:
-                print(f" - {var.name} = {value}")
-        print()
-
-        print("Constraints:")
-        for constraint in prob.constraints.values():
-            if constraint.value() != 0:
-                print(f" - {constraint.name}: {constraint.value()}")
-        print()
-
+    objective = prob.objective.value()
     used_links_per_track = {}
     for track_id in tracks.keys():
         used_links_per_track[track_id] = [
             link for link, var in link_usages[track_id].items() if var.varValue > 0]
-    return status, used_links_per_track
+    return MultiTrackSolution.found(objective, used_links_per_track)
 
-def get_multi_track_optimizer(type: str) -> Callable[[Network, dict[str, Track], bool], tuple[int, dict[str, list[tuple[str, str]]]]]:
+
+def get_multi_track_optimizer(type: str) -> Callable[[Network, dict[str, Track], bool], MultiTrackSolution]:
     if type == "single":
         return multi_to_single_track_adapter
     elif type == "multiple":
