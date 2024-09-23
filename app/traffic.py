39b7e5861f3308f0924ccd6a1a228166aa01835b
Zotyamester <szatmary.zoltan1222@gmail.com>
2024-09-23 23:32:55 +0200

    Add continent-based relay generator

--- app/traffic.py
+++ app/traffic.py
@@ -27,35 +27,36 @@ def generate_broadcast_traffic(track_id: str, publisher: str, subscribers: list[
     return tracks
 
 
-def generate_circle_edge_relays(network: nx.DiGraph, count: int) -> list[tuple[str, dict[str, tuple[float, float]]]]:
-    latitudes = [lat for lat, _ in nx.get_node_attributes(network, "location").values()]
-    longitudes = [lon for _, lon in nx.get_node_attributes(network, "location").values()]
-    bounding_box = (min(latitudes), max(latitudes), min(longitudes), max(longitudes))
+def generate_continental_relays(number_of_continents: int, relay_per_continent: int, seed: int | None = None) -> list[tuple[str, dict[str, tuple[float, float]]]]:
+    if seed is not None:
+        random.seed(seed)
+    
+    CONTINENTS = {
+        "North_Europe": {"lat_range": (50, 70), "lon_range": (-10, 30)},
+        "South_Europe": {"lat_range": (35, 50), "lon_range": (-10, 40)},
+        "North_America": {"lat_range": (25, 50), "lon_range": (-130, -60)},
+        "South_America": {"lat_range": (-55, 10), "lon_range": (-80, -35)},
+        "Asia": {"lat_range": (10, 60), "lon_range": (60, 150)},
+        "Africa": {"lat_range": (-35, 35), "lon_range": (-20, 55)},
+        "Australia": {"lat_range": (-50, -10), "lon_range": (110, 160)},
+    }
 
-    inscribed_circle_radius = min(
-        (bounding_box[1] - bounding_box[0]) / 2,
-        (bounding_box[3] - bounding_box[2]) / 2
-    )
-    inscribed_circle_center = (
-        (bounding_box[0] + bounding_box[1]) / 2,
-        (bounding_box[2] + bounding_box[3]) / 2
-    )
+    def generate_location(continent, ranges):
+        lat = random.uniform(*ranges["lat_range"])
+        lon = random.uniform(*ranges["lon_range"])
+        return (continent, {"location": (round(lat, 4), round(lon, 4))})
 
     relays = []
-    
-    for i in range(0, count):
-        angle = 2 * 3.14159 * i / count
-        lat = inscribed_circle_center[0] + inscribed_circle_radius * math.cos(angle)
-        lon = inscribed_circle_center[1] + inscribed_circle_radius * math.sin(angle)
-        relays.append((f"relay{i}", {"location": (lat, lon)}))
-
+    for continent, ranges in random.sample(list(CONTINENTS.items()), k=number_of_continents):
+        for i in range(relay_per_continent):
+            relays.append(generate_location(f"{continent}-{i}", ranges))
     return relays
 
 
 def generate_point_of_presence_relays(network: nx.DiGraph) -> list[tuple[str, dict[str, tuple[float, float]]]]:
     relays = []
 
-    for i, (node, attrs) in enumerate(network.nodes(data=True)):
+    for i, (_, attrs) in enumerate(network.nodes(data=True)):
         relays.append((f"relay{i}", {"location": attrs["location"]}))
 
     return relays
